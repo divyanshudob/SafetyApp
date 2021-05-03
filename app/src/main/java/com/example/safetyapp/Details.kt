fcef9a82a908a4e39879d40eaa5b7181c9371b1f
Divyanshu <divyanshudob@gmail.com>
2021-05-03 20:51:48 +0530

    commit1

--- app/src/main/java/com/example/safetyapp/Details.kt
+++ app/src/main/java/com/example/safetyapp/Details.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
@@ -16,8 +17,16 @@ class Details : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
         val Add = findViewById<Button>(R.id.add)
+        val phoneNo = findViewById<EditText>(R.id.PhoneNumber)
         Add.setOnClickListener{
-            Toast.makeText(this,"Number Added",Toast.LENGTH_SHORT).show()
+            if(phoneNo.toString()== null)
+            {
+                Toast.makeText(this,"Phone Number can't be Null", Toast.LENGTH_SHORT).show()
+            }
+
+            else {
+                Toast.makeText(this, "Number Added", Toast.LENGTH_SHORT).show()
+            }
         }
 
 
